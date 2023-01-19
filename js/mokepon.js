d26b66134e6637ecc9933457f72012be54d6cacf
Stevenson G <didiermaxilo3@gmail.com>
2023-01-19 15:55:57 -0500

    MokeponGame v1.0

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,3 +1,23 @@
+/* Variables getted of the HTML document */
+const cardsContainer = document.getElementById('cardsContainer')
+const atkSelectionSection = document.getElementById('attack-selection')
+const playerPetButton = document.getElementById('petButton')
+const restartButton = document.getElementById('restartButton')
+const petSelectionSection = document.getElementById('pet-selection')
+const playerPetSpan = document.getElementById('playerPet')
+const enemyPetSpan = document.getElementById('enemyPet')
+const playerImg = document.getElementById('playerImg')
+const enemyImg = document.getElementById('enemyImg')
+const playerLivesSpan = document.getElementById('playerLives')
+const enemyLivesSpan = document.getElementById('enemyLives')
+const battleResult = document.getElementById('battle-result')
+const playerFightAtk = document.getElementById('player-atk')
+const enemyFightAtk = document.getElementById('enemy-atk')
+const attackButtons = document.getElementById('attack-buttons')
+const mapSection = document.getElementById('viewMap')
+const map = document.getElementById('map')
+map.width = window.innerWidth*0.6
+map.height = window.innerWidth*0.45
 /* Variables used for the JS code */
 let playerLives = 3
 let playerAtks = []
@@ -17,40 +37,47 @@ let fireButton
 let watterButton
 let groundButton
 let buttons = []
-
-/* Variables getted of the HTML document */
-const cardsContainer = document.getElementById('cardsContainer')
-const atkSelectionSection = document.getElementById('attack-selection')
-const playerPetButton = document.getElementById('petButton')
-const restartButton = document.getElementById('restartButton')
-const petSelectionSection = document.getElementById('pet-selection')
-const playerPetSpan = document.getElementById('playerPet')
-const enemyPetSpan = document.getElementById('enemyPet')
-const playerImg = document.getElementById('playerImg')
-const enemyImg = document.getElementById('enemyImg')
-const playerLivesSpan = document.getElementById('playerLives')
-const enemyLivesSpan = document.getElementById('enemyLives')
-const battleResult = document.getElementById('battle-result')
-const playerFightAtk = document.getElementById('player-atk')
-const enemyFightAtk = document.getElementById('enemy-atk')
-const attackButtons = document.getElementById('attack-buttons')
+let canvas2D = map.getContext("2d")
+let interval
+let backgroundMapImg = new Image()
+backgroundMapImg.src = './assets/mokemap.png'
 
 //Class, pets and attacks definition
 class Mokepon{
-    constructor(newName, newPhoto, newLive){
+    constructor(newName, newPhoto, newLive, mapPhoto, x = 10, y = 10){
         this.name = newName
         this.photo = newPhoto
         this.live = newLive
         this.attacks = []
+        this.x = x
+        this.y = y
+        //Size default of the img for move later
+        this.width = map.width/8
+        this.height = this.width
+        this.photoMove = new Image()
+        this.photoMove.src = mapPhoto
+        this.xVelocity = 0
+        this.yVelocity = 0
+    }
+
+    //Metodh of the class/object that make the draw at the canvas
+    drawSelf(){
+        canvas2D.drawImage(
+            this.photoMove,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+    )
     }
 }
 
-let hipodoge = new Mokepon('Hipodoge', './assets/mokepons_mokepon_hipodoge_attack.png', 5)
-let capipepo = new Mokepon('Capipepo', './assets/mokepons_mokepon_capipepo_attack.png', 5)
-let ratigueya = new Mokepon('Ratigueya', './assets/mokepons_mokepon_ratigueya_attack.png', 5)
-let langostelvis = new Mokepon('Langostelvis', './assets/mokepons_mokepon_langostelvis_attack.png', 5)
-let pydos = new Mokepon('Pydos', './assets/mokepons_mokepon_pydos_attack.png', 5)
-let tucapalma = new Mokepon('Tucapalma', './assets/mokepons_mokepon_tucapalma_attack.png', 5)
+let hipodoge = new Mokepon('Hipodoge', './assets/mokepons_mokepon_hipodoge_attack.png', 5, './assets/hipodoge.png')
+let capipepo = new Mokepon('Capipepo', './assets/mokepons_mokepon_capipepo_attack.png', 5, './assets/capipepo.png')
+let ratigueya = new Mokepon('Ratigueya', './assets/mokepons_mokepon_ratigueya_attack.png', 5, './assets/ratigueya.png')
+let langostelvis = new Mokepon('Langostelvis', './assets/mokepons_mokepon_langostelvis_attack.png', 5, './assets/langostelvis.png')
+let pydos = new Mokepon('Pydos', './assets/mokepons_mokepon_pydos_attack.png', 5, './assets/pydos.png')
+let tucapalma = new Mokepon('Tucapalma', './assets/mokepons_mokepon_tucapalma_attack.png', 5, './assets/tucapalma.png')
 
 hipodoge.attacks.push(
     {name: 'WATTER💧', id: 'watter'},
@@ -97,7 +124,29 @@ langostelvis.attacks.push(
 
 mokepons.push(hipodoge, capipepo, ratigueya, pydos, tucapalma, langostelvis)
 
-function startGame() {    
+//Definition of Enemy Mokepons (only 3 because I want xD) and their random positions
+let enemyMokepons = []
+//For cicle to Copy every original mokepon for a "copy" to Enemy Mokepons using Object.assign
+mokepons.forEach(mokepon => {
+    let newMokepon = new Mokepon()
+    newMokepon = Object.assign(newMokepon, mokepon)
+    enemyMokepons.push(newMokepon)
+});
+//Delete 3 random Mokepons of the Enemy Mokepons array
+for (let i = 0; i < 3; i++) {
+    let enemy = random(0,enemyMokepons.length-1)
+    enemyMokepons.splice(enemy,1)
+}
+//Add a Random position for every Enemy Mokepon
+enemyMokepons.forEach(mokepon => {
+    mokepon.x = random(mokepon.width+2, map.width-mokepon.width)
+    mokepon.y = random(mokepon.height+2, map.height-mokepon.height)
+})
+
+////////============================================================================////////
+//Start the functions at the order of the game line
+function startGame(){
+    mapSection.style.display = 'none'
     mokepons.forEach((mokepon) =>{
         mokeponCard = `
             <input type="radio" name="pet" id=${mokepon.name} />
@@ -123,63 +172,58 @@ function startGame() {
     })
 }
 
-function playerPetSelect() {
-    petSelectionSection.style.display = 'none'
-    atkSelectionSection.style.display = 'flex'
+function playerPetSelect(){
+    petSelectionSection.style.display = 'none'   
+    mapSection.style.display = 'flex'
     
-    /* Check every input and put the Pet name at playerPet variable for "print" its attacks latter 
-        I add and "else" when don't select any pet and reload the game */
+    /* Check every input for copy the Pet Object at PlayerPet (when don't select any pet and reload the game
+        and put the Pet name at playerPetSpan variable for "print" its attacks latter  */
+    playerPet = new Mokepon()
     if (inputHipodoge.checked) {
-        playerPetSpan.innerHTML = inputHipodoge.id
-        playerPet = mokepons[0]
+        playerPet = Object.assign(playerPet, mokepons[0])
     } else if (inputCapipepo.checked) {
-        playerPetSpan.innerHTML = inputCapipepo.id
-        playerPet = mokepons[1]
+        playerPet = Object.assign(playerPet, mokepons[1])
     } else if (inputRatigueya.checked) {
-        playerPetSpan.innerHTML = inputRatigueya.id
-        playerPet = mokepons[2]
+        playerPet = Object.assign(playerPet, mokepons[2])
     } else if (inputPydos.checked) {
-        playerPetSpan.innerHTML = inputPydos.id
-        playerPet = mokepons[3]
+        playerPet = Object.assign(playerPet, mokepons[3])
     }else if (inputTucapalma.checked) {
-        playerPetSpan.innerHTML = inputTucapalma.id
-        playerPet = mokepons[4]
+        playerPet = Object.assign(playerPet, mokepons[4])
     }else if (inputLangostelvis.checked) {
-        playerPetSpan.innerHTML = inputLangostelvis.id
-        playerPet = mokepons[5]
+        playerPet = Object.assign(playerPet, mokepons[5])
     }else {
         alert("You DON'T select any pet")
         return location.reload()
     }
 
-    /* Enemy pet random select */
-    enemyPet = mokepons[random(1,mokepons.length)-1]
+    makeMap()
+}
+
+// Collect the sequence of the Player attacks and make the random enemy attacks
+function AtkSequence(enemyMokepon){
+    mapSection.style.display = 'none'
+    atkSelectionSection.style.display = 'flex'
+
+    playerPetSpan.innerHTML = playerPet.name
+
+    /* Enemy pet crashed */
+    enemyPet = enemyMokepon
     enemyPetSpan.innerHTML = enemyPet.name
 
     // Add Player and Enemy Mokepon elected image
     playerImg.innerHTML += `<img src=${playerPet.photo} alt=${playerPet.name}>`
     enemyImg.innerHTML += `<img src=${enemyPet.photo} alt=${enemyPet.name}>`
     
-
     /* Traverse the Player's Mokepon attacks array and take one by one and inject this attacks to the HTML 
-        the attackButtons is the DIV id where are the buttons*/
-            playerPet.attacks.forEach(attack => {
-                attackButtons.innerHTML += `
-                    <button id="${attack.id}" class="attack-button B_Atk">${attack.name}</button>
-                `
-            })
-
-    /*  */
-    fireButton = document.getElementById('fire')
-    watterButton = document.getElementById('watter')
-    groundButton = document.getElementById('ground')
+    the attackButtons is the DIV id where are the buttons*/
+    playerPet.attacks.forEach(attack => {
+        attackButtons.innerHTML += `
+            <button id="${attack.id}" class="attack-button B_Atk">${attack.name}</button>
+        `
+    })
 
     buttons = document.querySelectorAll('.B_Atk')
-    AtkSequence()
-}
 
-// Collect the sequence of the Player attacks and make the random enemy attacks
-function AtkSequence(){
     buttons.forEach((button) => {
         button.addEventListener('click', (eve) =>{
             playerAtks.push(eve.target.textContent)
@@ -203,7 +247,10 @@ function enemyRandomAtk(){
 
 function combat(){
     for(let i = 0; i < 5; i++){
-        if((playerAtks[i] == 'FIRE🔥' && enemyAtks[i] == 'GROUND🌱') || (playerAtks[i] == 'WATTER💧' && enemyAtks[i] == 'FIRE🔥') || (playerAtks[i] == 'GROUND🌱' && enemyAtks[i] == 'WATTER💧')) {
+        if (playerAtks[i] == enemyAtks[i]) {
+            messageCreate("TIE :/", playerAtks[i], enemyAtks[i])
+            playerLivesSpan.innerHTML = playerLives
+        }else if((playerAtks[i] == 'FIRE🔥' && enemyAtks[i] == 'GROUND🌱') || (playerAtks[i] == 'WATTER💧' && enemyAtks[i] == 'FIRE🔥') || (playerAtks[i] == 'GROUND🌱' && enemyAtks[i] == 'WATTER💧')) {
             messageCreate("WIN!", playerAtks[i], enemyAtks[i])
             enemyLives--
             enemyLivesSpan.innerHTML = enemyLives
@@ -239,6 +286,91 @@ function messageCreate(result, playerAtk, enemyAtk){
 function finalMessageCreate(finalResult) {
     battleResult.innerHTML = finalResult
     restartButton.style.display = 'block'
+}
+
+function makeMap(){
+    //draw the img at the initial position
+    drawCanvas(0,0)
+    interval = setInterval(moveCanvas, 100)
+    //Code for check the key pressed and change the img position
+    document.addEventListener("keydown", function(keyboard){
+        switch (keyboard.key) {
+            case "ArrowUp":
+            case "w":
+                drawCanvas(0,-10)
+                break;
+            case "ArrowDown":
+            case "s":
+                drawCanvas(0,10)
+                break;
+            case "ArrowRight":
+            case "d":
+                drawCanvas(10,0)
+                break;
+            case "ArrowLeft":
+            case "a":
+                drawCanvas(-10,0)
+                break;
+            default:
+                break;
+        }
+    })
+}
+
+function drawCanvas(x,y){
+    //change the attribute of the Object for move the img
+    playerPet.x += x
+    playerPet.y += y
+    //Clean the canvas from 0,0 to canvas/map "end", before draw new positioned img
+    canvas2D.clearRect(0, 0, map.width, map.height)
+    canvas2D.drawImage(backgroundMapImg, 0, 0, map.width, map.height)
+    enemyMokepons.forEach(mokepon => {
+        mokepon.drawSelf()
+    })
+    playerPet.drawSelf()
+}
+
+function changePetVelocity(x,y){
+    playerPet.xVelocity = x
+    playerPet.yVelocity = y
+    moveCanvas()
+}
+
+function moveCanvas(){
+    //change the attribute of the Object for move the img
+    playerPet.x += playerPet.xVelocity
+    playerPet.y += playerPet.yVelocity
+    //Clean the canvas from 0,0 to canvas/map "end", before draw new positioned img
+    canvas2D.clearRect(0, 0, map.width, map.height)
+    
+    for (let i = 0; i < 3; i++) {
+        if((playerPet.x < enemyMokepons[i].x+playerPet.width &&
+            playerPet.y < enemyMokepons[i].y+playerPet.width) &&
+            (playerPet.x+playerPet.width > enemyMokepons[i].x &&
+            playerPet.y+playerPet.width > enemyMokepons[i].y)){
+                AtkSequence(enemyMokepons[i])
+                clearInterval(interval)
+        }else{
+        }
+    }
+
+    //Form made by course teacher 
+    /* for (let i = 0; i < 3; i++) {
+        if(playerPet.x > enemyMokepons[i].x+100 ||
+            playerPet.x+100 < enemyMokepons[i].x ||
+            playerPet.y > enemyMokepons[i].y+100 ||
+            playerPet.y+100 < enemyMokepons[i].y){
+        }else{
+            console.log("PETS CRASHHEDDDD")
+        }
+    } */
+    
+    canvas2D.drawImage(backgroundMapImg, 0, 0, map.width, map.height)
+    enemyMokepons.forEach(mokepon => {
+        mokepon.drawSelf()
+    })
+    playerPet.drawSelf()
+
 }
 
 function random(min, max) {
